a0fbb6266a17a823a3d066a3f3a931c6834de883
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-11-16 21:13:59 -0300

    bradesco-hbtn-spring - spring-mocking/1x02

--- spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario> findById(Long id);
+
+    Usuario save(Usuario usuario);
 }
